e488f81f854593af2818e5a2ded43f830a386a1a
zkiddy <120651969+zkiddy@users.noreply.github.com>
2023-02-13 13:27:56 +0700

    Create level_rows.sql

---
+++ Format_Search/level_rows.sql
@@ -0,0 +1,9 @@
+-- first method
+SELECT $[$38.234000373.Number] - $[$38.34.Number]
+
+-- second method
+Declare @Price1 as Money
+Declare @Price2 as Money
+SET @Price1 = (SELECT $[$38.U_TEST1.number])
+SET @Price2 = (SELECT $[$38.U_TEST2.number])
+SELECT @Price1 * @Price2
