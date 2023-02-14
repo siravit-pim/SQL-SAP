d7dedb35e1e3923145389bca6bc16932f7ed2e10
zkiddy <120651969+zkiddy@users.noreply.github.com>
2023-02-14 09:43:32 +0700

    Create group_multiRows.sql

---
+++ Just_Query/group_multiRows.sql
@@ -0,0 +1,7 @@
+stuff ( (  select ',' + cast(b.NumAtCard  as nvarchar(max))					
+		from OPCH b 
+		group by b.NumAtCard
+		FOR XML PATH ('') ), 1, 1, ''
+	) as 'INVNo'
+  
+  -- ref : https://medium.com/t-t-software-solution/mssql-รวมข้อมูลหลายๆ-rows-ให้อยู่ใน-1-column-ด้วย-sql-for-xml-path-fb2b3456668f
