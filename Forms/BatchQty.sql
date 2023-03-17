7099f74ad7912917b0411c7ba0c1c772f05c4555
Siravit Pimaksorn <120651969+siravit-pim@users.noreply.github.com>
2023-03-17 13:18:31 +0700

    Create BatchQty.sql

---
+++ Forms/BatchQty.sql
@@ -0,0 +1,12 @@
+--information Batch with Qty
+select T0.Itemcode,T0.ItemName
+	,OBTQ.Quantity,OBTQ.WhsCode
+	,T0.MnfDate,T0.ExpDate
+	,T0.DistNumber,T0.MnfSerial
+	,OITM.InvntryUom
+from obtn T0
+join OBTQ on (OBTQ.Quantity > 0) and T0.Itemcode = OBTQ.ItemCode 
+	and (T0.SysNumber = OBTQ.SysNumber and T0.AbsEntry = OBTQ.MdAbsEntry)
+left join OITM on T0.ItemCode = OITM.ItemCode
+--where T0.itemcode = 'RM-TG1000NE'
+order by 1,DistNumber,MnfSerial
