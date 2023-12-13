a52bf8e56a58a7dbd12ba427b696f38d4839055b
zKiddy <120651969+siravit-pim@users.noreply.github.com>
2023-12-13 09:32:16 +0700

    Update and rename stand_alone_CN.sql to StandAlone_CN.sql

--- Approval/StandAlone_CN.sql
+++ Approval/StandAlone_CN.sql
@@ -1,3 +1,3 @@
--- if stand alone CN that approveal (not copy from Return Request)
+-- if stand  alone CN that approveal (not copy from Return Request)
 SELECT DISTINCT 'TRUE'
 WHERE $[$38.43.0] <> 234000031 --Type Return Return
