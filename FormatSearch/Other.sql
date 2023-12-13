c2256070b456ae09342c553912d98e3ecf0ca976
zKiddy <120651969+siravit-pim@users.noreply.github.com>
2023-12-13 09:50:33 +0700

    Update Other.sql

--- FormatSearch/Other.sql
+++ FormatSearch/Other.sql
@@ -26,3 +26,14 @@ left join ORPC on $[$25.1] = ORPC.[DocNum]
 -- direct calculated
 SELECT ( $[$0_U_G.C_0_3.Number] * $[$0_U_G.C_0_7.Number] ) / 100.0
 -----------------------------------
+
+/* Can calculate with 2 method and can use decalare in FS, it's STD */
+-- first method
+SELECT $[$38.234000373.Number] - $[$38.34.Number]
+
+-- second method
+Declare @Price1 as Money
+Declare @Price2 as Money
+SET @Price1 = (SELECT $[$38.U_TEST1.number])
+SET @Price2 = (SELECT $[$38.U_TEST2.number])
+SELECT @Price1 * @Price2
